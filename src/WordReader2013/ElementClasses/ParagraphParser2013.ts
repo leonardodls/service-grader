445863668f8ba5ed8285b6809aebaa5bcb4330ba
chauhanvipul25 <vipul.chauhan@comprotechnologies.com>
2024-04-19 12:47:32 +0530

    minor file name fix

--- src/WordReader2013/ElementClasses/ParagraphParser2013.ts
+++ src/WordReader2013/ElementClasses/ParagraphParser2013.ts
@@ -1,3 +1,3 @@
-import { ParagraphParser2010 } from "../../WordReader2010/ElementParser/ParagraphParser2010 ";
+import { ParagraphParser2010 } from "../../WordReader2010/ElementParser/ParagraphParser2010";
 
 export class ParagraphParser2013 extends ParagraphParser2010 {}
